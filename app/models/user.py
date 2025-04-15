58190b7d44127e9d77ddba1d4f33b605bfba1310
Suttiago <tiagobsversut@gmail.com>
2025-04-14 23:42:49 -0300

    class usuario

--- app/models/user.py
+++ app/models/user.py
@@ -1,1 +1,8 @@
  
+class Usuario():
+    def __init__(self, nome,cpf,renda_mensal,data_nascimento,trabalho):
+        self.nome = nome
+        self.cpf = cpf
+        self.renda_mensal = renda_mensal
+        self.trabalho = trabalho
+        self.data_nascimento = data_nascimento
