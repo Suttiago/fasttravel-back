31615713f3dd3d6c03ad015e76e85a999d6987a0
Suttiago <tiagobsversut@gmail.com>
2025-04-20 00:42:20 -0300

    conexao com banco e começo do controller

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
- 
+from database.db import Database 
+from dotenv import load_dotenv
+import os
+
+
+
+#conexao banco
+load_dotenv()
+
+banco = Database(
+    usuario = os.getenv('DB_USER'),
+    senha = os.getenv('DB_PASSWORD'),
+    host = os.getenv('DB_HOST'),
+    porta = os.getenv('DB_PORT'),
+    nome_banco = os.getenv('DB_BAME')
+)
+banco.conectar_banco()
+
+
+
