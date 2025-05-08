f04fe865d1856290bf666f49379cf37104f8ae27
Suttiago <tiagobsversut@gmail.com>
2025-05-07 23:33:13 -0300

    crud usuario

--- app/main.py
+++ app/main.py
@@ -3,9 +3,16 @@ from dotenv import load_dotenv
 from flask import Blueprint, render_template, request, redirect, url_for, app,Flask
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 
+@app.route('/')
+def index():
+    return render_template('form.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
 
 
 
--- app/models/user.py
+++ app/models/user.py
@@ -1,23 +1,13 @@
 from pydantic import BaseModel
-from typing import List
 from datetime import date
 from models.orcamento import Orcamento
 
 class Usuario(BaseModel):
     nome : str 
     cpf : str
+    email : str
+    senha : str
     renda_mensal : float
     trabalho : str
     data_nascimento : date
-    
-    def novo_orcamento(self, destino: str, check_in: date, check_out: date, adultos: int, criancas: int):
-        novo_orcamento = Orcamento(
-            destino=destino,
-            check_in=check_in,
-            check_out=check_out,
-            adultos=adultos,
-            criancas=criancas
-        )
-        self.orcamentos.append(novo_orcamento)
-        return novo_orcamento
     
