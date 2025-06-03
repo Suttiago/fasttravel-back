68cb5d5ac882a334c24782252a8e5d3462075d61
Suttiago <150452828+Suttiago@users.noreply.github.com>
2025-06-03 16:45:44 -0300

    Update ContaReceber.py

--- app/models/ContaReceber.py
+++ app/models/ContaReceber.py
@@ -1,12 +1,21 @@
-from sqlalchemy import Column, Integer, Date, String, Float
+from sqlalchemy import Column, Integer, Float, String, ForeignKey, Date
 from database.db import Base
+from sqlalchemy.orm import relationship
 
 class ContaReceber(Base):
     __tablename__ = 'CONTAS_RECEBER'
-    
-    id = Column(Integer, primary_key = True)
-    valor = Column(Float, nullable=True)
-    dt_vencimento = Column(Date, nullable=True)
-    tipo_pagamento = Column(String, nullable= True)
-    numero_parcelas = Column(Float, nullable=True)
-    status = Column(String, nullable=True)
+
+    id = Column(Integer, primary_key=True)
+    valor = Column(Float, nullable=False)
+    dt_vencimento = Column(Date, nullable=False)
+    metodo_pagamento = Column(String, nullable=False)
+    n_parcelas = Column(Integer, nullable=False)
+    status = Column(String, nullable=False)
+
+    # FK para orçamento
+    orcamento_id = Column(Integer, ForeignKey('ORCAMENTOS.id'))
+    orcamento = relationship("Orcamento", back_populates="contas_receber")
+
+    # FK para plano de pagamento
+    plano_pagamento_id = Column(Integer, ForeignKey('PLANOS_PAGAMENTO.id'))
+    plano_pagamento = relationship("PlanoPagamento", back_populates="contas_receber")
