808d41d776b3fb9a744f7f321d5aa84a554e46f0
Suttiago <150452828+Suttiago@users.noreply.github.com>
2025-06-03 16:13:15 -0300

    Update Orcamento.py

--- app/models/Orcamento.py
+++ app/models/Orcamento.py
@@ -1,19 +1,17 @@
-from sqlalchemy import Column, Integer, Date, String, ForeignKey
+from sqlalchemy import Column, Integer, Float, String, ForeignKey
 from database.db import Base
-from sqlalchemy.orm import  relationship
+from sqlalchemy.orm import relationship
 
 class Orcamento(Base):
     __tablename__ = 'ORCAMENTOS'
-    
+
     id = Column(Integer, primary_key=True)
-    check_in = Column(Date,nullable=False)
-    check_out = Column(Date,nullable=False)
-    adultos = Column(Integer, nullable=False)
-    criancas = Column(Integer, nullable=False)
-    Status = Column(String, nullable=False)
-    
-    usuario_id = Column(Integer,ForeignKey('USUARIOS.ID'),nullable=False)
-    usuario = relationship("Usuario", back_populates="orcamentos")
+    valor_passagens = Column(Float, nullable=False)
+    valor_hoteis = Column(Float, nullable=False)
+    valor_total = Column(Float, nullable=False)
+    status = Column(String, nullable=False)
 
     destino_id = Column(Integer, ForeignKey('DESTINOS.id'))
     destino = relationship("Destino", back_populates="orcamentos")
+
+    contas_receber = relationship("ContaReceber", back_populates="orcamento", cascade="all, delete-orphan")
